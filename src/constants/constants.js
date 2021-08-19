6f86fa4602b8bc2bb7e946363a1ec1989a7fe10d
spencer1573 <spencer1573@users.noreply.github.com>
2021-08-18 20:51:33 -0600

    added some images to be ready for processing in constants adjusted root of index

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -17,7 +17,7 @@ export const texture = [
     imgSrc:
       '//cdn.shopify.com/s/files/1/0594/0848/2477/t/3/assets/subscription-slider_rounds_400x400.png?v=1128720733247166019',
   },
-];
+]
 
 // export const groundCoffees = [
 //   {
@@ -62,4 +62,17 @@ export const summarySteps = [
     initImgSrc:
       '//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/coffeeClubSignUp_step6_200x200.png?v=3796666373239231893',
   },
-];
+]
+
+// images to hook up
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/coffeeClubSignUp_step4_200x200.png?v=10610913660879988772" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/coffeeClubSignUp_step5_200x200.png?v=6894272251298571702" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/coffeeClubSignUp_step6_200x200.png?v=3796666373239231893" >
+
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/GUNSHIP_BAG_UPDATE-1200.png?v=6516688591982501407" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/lava_panther_540x.png?v=12436704304646100494" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/space_bear_1024x1024@2x.png?v=14534080239009770454" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/subscription-slider_1-bag_400x400.png?v=9099848038866881308" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/subscription-slider_2-bag_400x400.png?v=4106248448494023126" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/subscription-slider_3-bag_400x400.png?v=10576128729452119620" >
+//     <img class="w-40 object-contain" src="//cdn.shopify.com/s/files/1/0594/0848/2477/t/1/assets/subscription-slider_4-bag_400x400.png?v=4968723301670925721" ></img>
